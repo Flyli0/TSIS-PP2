336338f7f116ae5873eb29709a10e9aa082c8519
Flyli0 <kirillrozkov653@gmail.com>
2025-01-13 21:20:52 +0500

    final

--- TSIS 1/W3Examples.py
+++ TSIS 1/W3Examples.py
@@ -38,3 +38,30 @@ print(type(x)) #type definig exmpl
 x = 1    # int
 y = 2.8  # float
 z = 1j   # complex
+
+'''Casting'''
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+'''Strings'''
+print("Hello")
+print('Hello')
+
+b = "Hello, World!"
+print(b[2:5]) #Slicing
+
+a = "Hello, World!"
+print(a.upper()) #Modifying by method .upper
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #Concatenation
+
+age = 36
+txt = f"My name is John, I am {age}" #formatting strings
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north." #escape char \
+
